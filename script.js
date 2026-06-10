d7519e4b176c595d5da572a58774af46dc78c518
waldaotto <waldaotto@gmail.com>
2026-06-10 10:40:21 -0300

    Menu responsivo

--- script.js
+++ script.js
@@ -3,10 +3,22 @@ function nav(){
 
     const temClasse = nav.classList.contains('open');
     if (temClasse){
-        nav.classList.toggle('open')
+
+        nav.classList.toggle('open');
+
+        let botao = document.getElementById('btn-menu');
+        botao.innerHTML = `<i class="fa fa-bars" aria-hidden="true">`;
+
+        let header = document.getElementById("header");
+        header.classList.toggle('open');
     }
     else{
-        nav.classList.add('open')
+        nav.classList.add('open');
+        let botao = document.getElementById('btn-menu');
+        botao.innerHTML = "X";
+
+        let header = document.getElementById("header");
+        header.classList.add('open');
     }
     
 }
